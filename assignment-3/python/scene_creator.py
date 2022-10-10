8aa2a8eb04b2f98da61de94d116032a3c4793b25
mmelkumyan <18melkumyanm@gmail.com>
2022-10-10 19:30:56 -0400

    adds scene creator script and .aliases file

---
+++ assignment-3/python/scene_creator.py
@@ -0,0 +1,7 @@
+import os
+
+asset_name = os.getenv('asset')
+
+scene_path = os.path.join("assets", asset_name, "maya", "scenes")
+print(f"Creating directory: {scene_path}")
+os.makedirs(scene_path)
