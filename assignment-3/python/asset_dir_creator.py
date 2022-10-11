56c31fbeab6ecb69d776aa73d7703251ff1a6c01
mmelkumyan <18melkumyanm@gmail.com>
2022-10-10 20:04:20 -0400

    Adds scene creator script

--- assignment-3/python/asset_dir_creator.py
+++ assignment-3/python/asset_dir_creator.py
@@ -1,7 +1,8 @@
+"""Creates directory 'assets/$asset/maya/scenes'"""
 import os
 
 asset_name = os.getenv('asset')
 
 scene_path = os.path.join("assets", asset_name, "maya", "scenes")
-print(f"Creating directory: {scene_path}")
+print(f"Creating directory: '{scene_path}'")
 os.makedirs(scene_path)
