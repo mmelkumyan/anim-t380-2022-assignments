db91c6c1a850651767df838076df695e40ba813d
mmelkumyan <18melkumyanm@gmail.com>
2022-10-26 13:43:56 -0400

    Cleans up filtering code

--- midterm/python/main.py
+++ midterm/python/main.py
@@ -21,27 +21,34 @@ import copy
 import imageio.v3 as iio
 from pathlib import Path
 
-"""
-Pseudo-code:
 
-- input parameters: frame directory, filters,
-
-- Set frame filter. {scene}_{shot}_{frame# starting at 1000}.{ext}
--- Can filter for scene, shot, frame range, or combo of all 3
-- Loop through each image in frames dir
--- Load in with OpenImageIo
--- Check for irregularities. If match, add to bad frames list:
---- file size is vastly different from previous (+- at least 25%?)
---- image is all black, or very close (image value < 5%. from HSV)
-- Print contents of bad frames list
-
-"""
+# Pseudo-code:
+#
+# - input parameters: frame directory, filters,
+#
+# - Set frame filter. {scene}_{shot}_{frame# starting at 1000}.{ext}
+# -- Can filter for scene, shot, frame range, or combo of all 3
+# - Loop through each image in frames dir
+# -- Load in with OpenImageIo
+# -- Check for irregularities. If match, add to bad frames list:
+# --- file size is vastly different from previous (+- at least 25%?)
+# --- image is all black, or very close (image value < 5%. from HSV)
+# - Print contents of bad frames list
 
 
-def load_naming_convention(naming_json_path: str):
-    with open(naming_json_path) as f:
-        data = json.load(f)
-    return data
+# def load_naming_convention(naming_json_path: str):
+#     with open(naming_json_path) as f:
+#         data = json.load(f)
+#     return data
+NAMING_CONVENTION_TXT = "./naming.txt"
+
+def load_naming_convention(naming_txt_path: str):
+    with open(naming_txt_path) as f:
+        line = f.readline()
+    file_name, ext = line.split(".")
+    name_words = file_name.split("_")
+    return  name_words, ext
+
 
 def get_filter_ranges(raw_filter):
     clean_filter = {}
@@ -67,38 +74,54 @@ def get_filter_ranges(raw_filter):
 
     return clean_filter
 
-def filter_images(directory, name_filter):
-    # Gather all images in directory
-    delimiter = "_"
-    images = {}
 
-    for file in Path(frame_dir).iterdir():
+def filter_images(directory: str, name_filter: dict, ext_filter: str):
+    # Gather all images in directory_
+    images = {}
+    for file in Path(directory).iterdir():
+        # Skip over directories
         if not file.is_file():
             continue
-        # try: # TODO: Try except on invalid named images. Give warning for bad names
 
-        ext = file.suffix
-        words = file.stem.split(delimiter)
-
-        if len(words) != len(name_filter):
-            print(f"Invalid named file found: {file.name}")
+        # Filter file extensions
+        ext = file.suffix.strip(".")
+        if ext != ext_filter:
             continue
 
-        for i, (word, range) in enumerate(name_filter.items()):
-            num = int(words[i])
-            if num < range["min"] or num > range["max"]:
+        # Get words from file name
+        file_name_words = file.stem.split("_")
+
+        # Ensure number of file name words is expected
+        if len(file_name_words) != len(name_filter):
+            print(f"Invalid named file found. Incorrect number of words: {file.name}")
+            continue
+
+        # Filter number ranges on file name
+        for i, num_range in enumerate(name_filter.values()):
+            # Ensure word is integer
+            try:
+                word_num = int(file_name_words[i])
+            except ValueError:
+                print(f"Invalid named file found. "
+                      f"Non-integer found: {file_name_words[i]} in {file.name}")
                 continue
 
+            # Filter integers out of range
+            if word_num < num_range["min"] or word_num > num_range["max"]:
+                continue
+
+        # Read in image
         images[file.name] = iio.imread(file)
     return images
 
+
 if __name__ == '__main__':
-    naming_words = load_naming_convention("naming.json")
+    naming_words, ext = load_naming_convention(NAMING_CONVENTION_TXT)
 
     parser = argparse.ArgumentParser(
         description="This tool helps identify broken frames.")
     parser.add_argument("frames_dir", help="Directory of frames", type=str)
-    for word in naming_words["naming"]:
+    for word in naming_words:
         parser.add_argument(f"--{word}", help="...", type=str)
     args = parser.parse_args()
 
@@ -108,7 +131,6 @@ if __name__ == '__main__':
 
     name_filter = get_filter_ranges(raw_filter)
 
-    filtered_images = filter_images(args.frames_dir, name_filter)
-    x=0
+    filtered_images = filter_images(args.frames_dir, name_filter, ext)
 
 # regex groupdicts in python docs
